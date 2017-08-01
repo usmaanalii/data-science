3bed9c412e9902fd0f234938ae540732c7d13e90
usyyy <usmaanalii@hotmail.co.uk>
2017-08-01 11:51:31 +0100

    Completed '09 Getting Data/Scraping the Web'
    - HTML parsing
    - O'Reilly Website Example

--- data-science-from-scratch/my-code/09-getting-data/03-scraping-the-web/02-ex-oreilly.py
+++ data-science-from-scratch/my-code/09-getting-data/03-scraping-the-web/02-ex-oreilly.py
@@ -1,7 +1,13 @@
 # * Example: O'Reilly Books About data
 #   - Examine how many data books O'Reilly has published over time
-from bs4 import BeautifulSoup
+#   NOTE: Most of this doesn't work since the demo html is different now
 import requests
+import json
+import matplotlib as plt
+from collections import Counter
+from bs4 import BeautifulSoup
+from time import sleep
+from dateutil.parser import parse
 
 url = "http://shop.oreilly.com/category/browse-subjects/" + \
       "data.do?sortby=publicationDate&page=1"
@@ -26,4 +32,77 @@ def is_video(td):
 print(len([td for td in tds if not is_video(td)]))
 
 # the book title is the text inside <a> inside <div class="thumbheader">
-title = td.find('div', 'thumbheader').a.text
+title = tds.find('div', 'thumbheader').a.text
+
+# author is in the text of the dic with AuthorName
+author_name = tds.find('div', 'AuthorName').text
+authors = [x.strip() for x in re.sub("^By ", "", author_name).split(",")]
+
+# ISBN seems to be contained in the link that's in the thumbheader <div>
+isbn_link = tds.find("div", "thumbheader").a.get("href")
+
+# re.match captures the part of the regex in parenthesis
+isbn = re.match("/product/(.*)\.do", isbn_link).group(1)
+
+# the dats is the contents of <span class="directorydate ">
+date = tds.find("span", "directorydate").text.strip()
+
+
+# Put this together into a function
+def book_info(td):
+    """given a BeautifulSoup <td> Tag representing a book.
+    extract the book's details and return a dict"""
+
+    title = td.find("div", "thumbheader").a.text
+    by_author = td.find("div", "AuthorName").text
+    authors = [x.strip() for x in re.sub("^By ", "", author_name).split(",")]
+    isbn_link = td.find("div", "thumbheader").a.get("href")
+    isbn = re.match("/product/(.*)\.do", isbn_link).group(1)
+    date = td.find("span", "directorydate").text.strip()
+
+    return {
+        "title": title,
+        "authors": authors,
+        "isbn": isbn,
+        "date": date
+    }
+
+
+# Now, we're ready to scrape
+base_url = "http://shop.oreilly.com/category/browse-subjects/" + \
+    "data.do?sortby=publicationDate&page="
+
+books = []
+
+NUM_PAGES = 31
+
+for page_num in range(1, NUM_PAGES + 1):
+    print("souping page", page_num, ",", len(books), " found so far")
+    url = base_url + str(page_num)
+    soup = BeautifulSoup(requests.get(url).text, 'html5lib')
+
+    for td in soup('td', 'thumbtext'):
+        if not is_video(td):
+            books.append(book_info(td))
+
+    sleep(30)
+
+
+# %% Plot the number of books published each year
+def get_year(book):
+    """book["date"] looks like 'November 2014' so we need to
+    split on the space and then take the second piece"""
+    return int(book["date"].split()[1])
+
+
+# 2014 is the last complete year of data (when this was made)
+year_counts = Counter([get_year(book) for book in books
+                      if get_year(book) <= 2014])
+
+years = sorted(year_counts)
+book_counts = [year_counts[year] for year in years]
+
+plt.plot(years, book_counts)
+plt.ylabel("# of data books")
+plt.title("Data is Big!")
+plt.show()
